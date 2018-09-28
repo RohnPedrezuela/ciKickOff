050dcc44030cd4c2cf60047c7cb014cc62b8c724
Rohn Pedrezuela <rpedrezuela@cambridge.org>
2018-09-28 21:57:26 +0800

    Commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello ");
-  res.send("World World");
+  res.send("Hello Rohn");
 });
 
 app.listen(4000);
